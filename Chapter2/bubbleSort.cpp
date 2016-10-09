6fce9cbc38763c4db15545cae6367f93bc784933
vito <vito.yanw@gmail.com>
2016-10-09 06:07:55 -0400

    add bubbleSort

---
+++ Chapter2/bubbleSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <sstream>
+
+#include "../util/util.h"
+
+int main()
+{
+    std::vector<int> input;
+    while(!getIntArrayFromConsole(input));
+
+    for(std::size_t i = 0; i < input.size()-1; i++)
+    {
+       for(std::size_t j = 0; j < input.size() - i - 1; j++) 
+       {
+            if(input[j] > input[j+1])
+            {
+                int temp = input[j];
+                input[j] = input[j+1];
+                input[j+1] = temp;
+            }
+        }
+        std::vector<int>::iterator it = input.begin();
+        while(it != input.end())
+        {
+         std::cout<<*it<<" ";
+            it++;
+        }
+        std::cout<<std::endl;
+    }
+    return 0;
+}
