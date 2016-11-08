93ef2fbff4d7446d165e17f7fd89a6eb7de14e39
vito <vito.yanw@gmail.com>
2016-11-09 04:18:21 +0800

    update link/calculateSum

--- link/calculateSum.cpp
+++ link/calculateSum.cpp
@@ -11,22 +11,29 @@ class Node
 {
 public:
 	int data;
+	int carry;
 	Node *next;
+	Node()
+	{
+		data = 0;
+		next = NULL;
+		carry = 0;
+	}
 };
 
 
-int calculateSum(Node *head1, Node *head2)
+Node* calculateSum(Node *head1, Node *head2, Node* head3, int carry)
 {
-	if(!head1 && !head2)
+	if(!head1 && !head2 && carry == 0)
 		return 0;
 
-	int sum = 0;
+	Node *node = NULL;
+	Node *head = NULL;
 	Node *node1 = head1;
 	Node *node2 = head2;
-	int bit = 1;
-	while(node1 || node2)
-	{
-		int temp = 0;
+	int temp = 0;
+	if(node1 || node2)
+	{		
 		if(node1)
 		{
 			temp = node1->data;
@@ -37,47 +44,111 @@ int calculateSum(Node *head1, Node *head2)
 		{
 			temp = temp + node2->data;
 			node2 = node2->next;
-		}
-
-		sum = sum + temp*bit;
-		bit = 10*bit;
+		}					
 	}
 
-	return sum;
+	temp += carry;
+	node = new Node();
+	carry = temp/10;
+	node->data = carry ? temp%10 : temp;
+	node->next = head3;
+	if(carry || node1 || node2)
+	{
+		head = calculateSum(node1, node2, node, carry);
+	}
+	else
+		head = node;	
+
+	return head;
 }
 
-int calculateSumReverse(Node *head1, Node *head2)
+Node* padList(Node *head, int number)
+{
+	if(!head || number == 0)
+		return head;
+
+	while(number)
+	{
+		Node *node = new Node;
+		node->data = 0;
+		node->next = head;
+		head = node;
+		number--;
+	}
+	return head;
+}
+
+int calListLength(Node *head)
+{
+	int number = 0;
+	while(head)
+	{
+		number++;
+		head = head->next;
+	}
+
+	return number;
+}
+
+Node* addListHelper(Node *head1, Node *head2)
+{
+	if(!head1 && !head2)
+		return NULL;
+	
+
+	Node *sumLower = addListHelper(head1->next, head2->next);
+
+	int sum = head1->data + head2->data + (sumLower ? sumLower->carry:0);
+
+	int carry = sum/10;
+
+	Node* node = new Node;
+	if(carry)
+	{
+		node->data = sum%10;		
+		node->carry = carry;
+		
+	}
+	else
+	{
+		node->data = sum;
+	}
+	node->next = sumLower;
+	
+	return node;
+}
+
+
+
+Node* calculateSumReverse(Node *head1, Node *head2)
 {
 	if(!head1 && !head2)
 		return 0;
 
-	int sum = 0;
 	Node *node1 = head1;
 	Node *node2 = head2;
-	int op1 = 0, op2 = 0;
-	while(node1 || node2)
-	{
-		int temp = 0;
-		if(node1)
-		{
-			temp = node1->data;
-			node1 = node1->next;
-			op1 = op1*10 + temp;
-		}
+	int length1 = 0, length2 = 0;
 
-		temp = 0;
-		if(node2)
-		{
-			temp = temp + node2->data;
-			node2 = node2->next;
-			op2 = op2*10 + temp;
-		}
-		
+	length1 = calListLength(head1);
+	length2 = calListLength(head2);
+
+	if(length1 > length2)
+	{
+		node2 = padList(node2, length1 - length2);
+	}
+	else
+		node1 = padList(node1, length2 - length1);
+
+	Node* node = addListHelper(node1, node2);
+	if(node && node->carry != 0)
+	{
+		Node *temp = new Node;
+		temp->data = node->carry;
+		temp->next = node;
+		node = temp;
 	}
 
-	sum = op1 + op2;
-
-	return sum;
+	return node;
 }
 
 
@@ -92,11 +163,21 @@ int main()
 	Node* head2 = DataStructure::buildLink<int, Node>(input2);
     
 
-    int result1 = calculateSum(head1, head2);
+    Node* result1 = calculateSum(head1, head2, NULL, 0);
         
-   	std::cout<<result1<<" "<<std::endl;
+   	while(result1)
+   	{
+   		std::cout<<result1->data;
+   		result1 = result1->next;
+   	}
+   	std::cout<<std::endl;
 
-   	int result2 = calculateSumReverse(head1, head2);
+   	Node* result2 = calculateSumReverse(head1, head2);
         
-   	std::cout<<"reverse is "<<result2<<" "<<std::endl;
+   	while(result2)
+   	{
+   		std::cout<<result2->data;
+   		result2 = result2->next;
+   	}
+   	std::cout<<std::endl;
 }
