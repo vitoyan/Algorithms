019f5b37b26301197c992a804d083d025174269e
vito <vito.yanw@gmail.com>
2016-11-24 19:21:02 +0800

    add quicksort non recursive function

--- sorting/quickSort.cpp
+++ sorting/quickSort.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iterator>
 #include <sstream>
+#include <stack>
 
 #include "./util/util.h"
 
@@ -22,29 +23,70 @@ int partition(std::vector<int> &input, int p, int r)
 	return i;
 }
 
-void quickSort(std::vector<int> &input, int p, int r)
+void quickSortRecursively(std::vector<int> &input, int p, int r)
 {
 	if(p < r)
 	{
 		int q = partition(input, p, r);		  	
-		quickSort(input, p, q - 1 );
-		quickSort(input, q + 1, r);
+		quickSortRecursively(input, p, q - 1 );
+		quickSortRecursively(input, q + 1, r);
 	}	
 }
 
+struct node
+{
+public:
+	node(int s, int e):start(s), end(e)
+	{
+
+	}
+	int start;
+	int end;	
+};
+
+void quickSort(std::vector<int> &input)
+{
+	if(input.empty() || input.size == 1)
+	{
+		return;
+	}
+	node n(0, input.size() - 1);
+	std::stack<node> st;
+	st.push(n);
+	while(!st.empty())
+	{
+		n = st.top();
+		st.pop();
+		int q = partition(input, n.start, n.end);
+		if(q > (n.start + 1))
+			st.push(node(n.start, q - 1));
+		if(q < (n.end - 1))
+			st.push(node(q + 1, n.end));
+	}
+}
 
 
 int main()
 {
-    std::vector<int> input;
+    std::vector<int> input, input1;
     while(!getIntArrayFromConsole(input));
-    quickSort(input, 0, input.size() - 1);
+    input1 = input;
 
+    quickSortRecursively(input, 0, input.size() - 1);
     auto it = input.begin();
     while(it != input.end())
     {
     	std::cout<<*it<<" ";
     	it++;
+    }
+    std::cout<<std::endl;
+
+    quickSort(input1);
+    auto it2 = input1.begin();
+    while(it2 != input1.end())
+    {
+    	std::cout<<*it2<<" ";
+    	it2++;
     }
     std::cout<<std::endl;
 }
